a15d08f4a933b0ce2f2d2f9e7f4e73861f1355b2
pet-r-off <pda761@mail.ru>
2023-04-13 12:46:14 +0300

    *: add math.* library

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "math.h"
+
+int main() {
+    std::cout << "Hello" << std::endl;
+    std::cout << math(10,15) << std::endl;
+    hello_msg();
+
+    return 0;
+}
---
+++ src/math.h
@@ -0,0 +1,7 @@
+#ifndef _MATH_H_
+#define _MATH_H_
+
+int math(int a, int b);
+void hello_msg();
+
+#endif
